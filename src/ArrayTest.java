06542fa22debb53655b6c453a484bb9514ee67a7
CodesLong <3529357386@qq.com>
2020-02-16 12:17:41 +0800

    字符串测试

---
+++ src/ArrayTest.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.io.*;
+public class ArrayTest {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4};
+        for(int index = 0; index < arr.length;index++){
+            System.out.println(index);
+        }
+
+    }
+
+}
---
+++ src/test01/test.java
@@ -0,0 +1,8 @@
+package test01;
+import org.junit.Test;
+public class test {
+    @Test
+    public void text01(){
+        System.out.println("hello");
+    }
+}
